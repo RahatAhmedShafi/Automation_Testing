e24d864ef474654c83c679114c639b9764286366
Rahat Ahmed Shafi <124520003+RahatAhmedShafi@users.noreply.github.com>
2025-11-09 12:25:13 +0600

    different browser test

--- FirstJava/src/test/java/ChromeTest.java
+++ FirstJava/src/test/java/ChromeTest.java
@@ -1,12 +1,14 @@
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class ChromeTest {
 
     public static void main(String[] args){
 //        System.out.println("Hello Dhaka");
-        WebDriver driver=new ChromeDriver();
-        driver.get("https://www.bbc.com/news");
+//        WebDriver driver=new ChromeDriver();
+        WebDriver driver =new FirefoxDriver();
+        driver.get("https://www.google.com/");
         driver.manage().window().maximize();
         driver.quit();
     }
